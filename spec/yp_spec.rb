21279f0d8818d9ee8cc398758073272ee319f6f4
Andrew Hall <andyhall1138@gmail.com>
2017-01-02 11:59:54 +0000

    Add rspec given and update specs

--- spec/yp_spec.rb
+++ spec/yp_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 describe Yp::TransactionRequest do
 
-  TRANSACTION_FIELDS = {
+  TRANSACTION = {
       merchantID: '101380',
       action: 'SALE',
       type: '1',
@@ -15,72 +15,64 @@ describe Yp::TransactionRequest do
       cardExpiryDate: '1213'
   }
 
-  it 'has a version number' do
-    expect(Yp::VERSION).not_to be nil
+  context 'version number' do
+    Then { !Yp::VERSION.nil? }
   end
 
   describe 'serialize_params' do
-    it 'should return an empty string if no params' do
-      expect(Yp::TransactionRequest.serialize_params({})).to eq('')
+    context 'should return an empty string if no params' do
+      Then { Yp::TransactionRequest.serialize_params({}) == '' }
     end
 
-    it 'should return a url-encoded string for a multiple properties, sorted' +
-        ' alphabetically by key' do
-
-      expect(Yp::TransactionRequest.serialize_params({ key: 'value 123' }))
-          .to eq('key=value+123')
+    context 'url-encoded string for a single param, sorted' do
+      Given(:params) { { key: 'value 123' } }
+      When(:result)  { Yp::TransactionRequest.serialize_params(params) }
+      Then { result == 'key=value+123' }
     end
 
-    it 'should return a url-encoded string for a multiple properties, sorted' +
-        ' alphabetically by key' do
-
-      expect(Yp::TransactionRequest.serialize_params({
-          key: 'value 123', a_key: 'value 456'
-      })).to eq('a_key=value+456&key=value+123')
-
+    context 'multiple properties, sorted alphabetically by key' do
+      Given(:params) { { key: 'value 123', a_key: 'value 456' } }
+      When(:result)  { Yp::TransactionRequest.serialize_params(params) }
+      Then { result == 'a_key=value+456&key=value+123' }
     end
 
-    it 'should return a url-encoded string for a example transaction, sorted' +
-      ' alphabetically by key' do
-
-      expect(Yp::TransactionRequest.serialize_params(TRANSACTION_FIELDS))
-          .to eq('action=SALE&amount=2691&cardExpiryDate=1213&cardNumber=492' +
-              '9+4212+3460+0821&countryCode=826&currencyCode=826&merchantID=' +
-              '101380&orderRef=Signature+Test&transactionUnique=55f025addd3c' +
-              '2&type=1')
-
+    context 'transaction' do
+      When(:result) { Yp::TransactionRequest.serialize_params(TRANSACTION) }
+      Then do
+        result ==
+            'action=SALE&amount=2691&cardExpiryDate=1213&cardNumber=4929+42' +
+            '12+3460+0821&countryCode=826&currencyCode=826&merchantID=10138' +
+            '0&orderRef=Signature+Test&transactionUnique=55f025addd3c2&type' +
+            '=1'
+      end
     end
   end
 
-  describe 'digest' do
-    it 'should return the SHA-512 of an empty string' do
-      expect(Yp::TransactionRequest.digest(''))
-          .to eq('cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d3' +
-              '6ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327' +
-              'af927da3e')
+  describe 'sha-512 digest' do
+    context 'empty string' do
+      When(:result) { Yp::TransactionRequest.digest('') }
+      Then do
+        result ==
+            'cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9c' +
+            'e47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da' +
+            '3e'
+      end
     end
   end
 
   describe 'create_signing_hash' do
 
-    it 'should return a hash of the serialized keys with the signature' +
-        ' appended' do
-
-      expect(Yp::TransactionRequest.new(
-          'signature',
-          key: 'value 123',
-          a_key: 'value 456'
-      ).create_signing_hash)
-          .to eq('0adba88d7eaff7ecf7e04a891823edb5684b290f66d813dfdff09ba9f2' +
-              '48897f01c5bde63fdf705e7dee69238f65260dd1de73d4ac1c31dafd4150a' +
-              '543c7e987')
-
-      expect(Yp::TransactionRequest.new('DontTellAnyone', TRANSACTION_FIELDS)
-                 .create_signing_hash)
-          .to eq('4df549856fb4e35539c024a45a066392e13b74a8f3c724884f17dd69419'+
-              'f29dbb3bcde5ef467f14e80778b605893d27e84527bfad44de0ea429db4234'+
-              '68e5ee8')
-
+    context 'transaction with signature' do
+      Given(:transaction) do
+        Yp::TransactionRequest.new('DontTellAnyone', TRANSACTION)
+      end
+      When(:result) { transaction.create_signing_hash }
+      Then do
+        result ==
+            '4df549856fb4e35539c024a45a066392e13b74a8f3c724884f17dd69419f29d' +
+            'bb3bcde5ef467f14e80778b605893d27e84527bfad44de0ea429db423468e5e' +
+            'e8'
+      end
     end
   end
 end
--- yp-ruby.gemspec
+++ yp-ruby.gemspec
@@ -36,4 +36,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
   spec.add_development_dependency "vcr", "~> 3.0", ">= 3.0.1"
+  spec.add_development_dependency "rspec-given", "~> 3.8"
 end
