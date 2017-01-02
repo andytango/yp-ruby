2d24643747e83ced9332c631774ad35402097a07
Andrew Hall <andyhall1138@gmail.com>
2017-01-02 11:39:59 +0000

    Add VCR for recording http responses

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,9 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "yp"
+require "vcr"
+require "rspec/given"
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/cassettes'
+  c.configure_rspec_metadata!
+end
