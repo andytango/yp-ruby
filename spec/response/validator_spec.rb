d0ee3fa5cfb94908e417b6cfb2b3d763b1bb3dc9
Andrew Hall <andrew@rioja.io>
2017-02-23 22:04:32 +0000

    Tweak validator spec

--- spec/response/validator_spec.rb
+++ spec/response/validator_spec.rb
@@ -2,16 +2,15 @@ require 'spec_helper'
 require_relative '../yp_helper'
 
 describe Yp::Response::Validator do
+  Given(:signature) { 'Engine0Milk12Next' }
 
   context 'with a valid response hash' do
-    Given(:signature) { 'Engine0Milk12Next' }
     Given(:params) { VALID_PARAMS }
     When(:validator) { Yp::Response::Validator.new(params, signature) }
     Then { validator.valid? }
   end
 
   context 'with invalid signing key' do
-    Given(:signature) { 'Engine0Milk12Next' }
     Given(:params) { VALID_PARAMS.merge(signature: 'wrong') }
     Given(:error_class) { Yp::Response::Validator::InvalidSignatureError }
     When(:validator) { Yp::Response::Validator.new(params, signature) }
@@ -19,7 +18,6 @@ describe Yp::Response::Validator do
   end
 
   context 'with missing signing key' do
-    Given(:signature) { 'Engine0Milk12Next' }
     Given(:params) { VALID_PARAMS.reject { |k, _| k == :signature } }
     Given(:error_class) { Yp::Response::Validator::MissingSignatureError }
     When(:validator) { Yp::Response::Validator.new(params, signature) }
